63aaf1ae550b9c5045c4c87d8464ebb29fc8e8fd
Caglar <caglarsulun@hotmail.com>
2021-12-01 23:57:28 -0500

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+function callback (){
+    console.log("I was successfully clicked.")
+};
+
 function addingEventListener() {
+    document.querySelector('input').addEventListener('click', callback);
 }
+
+addingEventListener();
+
+
